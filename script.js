717dea85e954514f9bbbc0698fc9b11e6e16efce
GrantFinnegan <grantfinnegan@gmail.com>
2024-06-05 12:15:55 -0500

    create operate function
    operate function will be used to call the appropriate base arithmetic
    function given a string of the operator being used, and the numbers

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
+const ADDITION_OPERATOR = '+';
+const SUBTRACTION_OPERATOR = '-';
+const MULTIPLICATION_OPERATOR = 'x';
+const DIVISION_OPERATOR = '/';
+
 function add(a, b) {
     //Number.parseFloat used to ensure + operator performs addition rather
     //than concatenation in the event a string is passed
     return Number.parseFloat(a) + Number.parseFloat(b);
 }
 
-function subtract(a, b) {
-    return a - b;
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
 }
 
-function multiply(a, b) {
-    return a * b;
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
 }
 
 function divide(dividend, divisor) {
     return dividend / divisor;
+}
+
+
+function operate(operator, firstNumber, secondNumber) {
+    switch(operator) {
+        case ADDITION_OPERATOR:
+            return add(firstNumber, secondNumber);
+        case SUBTRACTION_OPERATOR:
+            return subtract(firstNumber, secondNumber);
+        case MULTIPLICATION_OPERATOR:
+            return multiply(firstNumber, secondNumber);
+        case DIVISION_OPERATOR:
+            return divide(firstNumber, secondNumber);
+    }
 }
